f3c098a4c6647a9a208bbff4bfaf4815b4abb0c0
juandav <e.juandav@gmail.com>
2017-03-23 17:58:03 -0500

    adding new route

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+'use strict'
 /*
   Module dependnecies
 */
 const express = require('express')
 const scragram = require('scragram')
+const bodyParser = require('body-parser')
+
 
 /*
   Constants
 */
 const app = express()
-const hashTag = 'streaming'
+const router = express.Router()
+const port = process.env.PORT || 8080
 
-// /hastag/?type=top
-app.get('/hashtag/', function (req, res) {
-  scragram.getLinksTopPost(hashTag, (err, data) => {
-    err ? res.json({ 'err': err }):res.json({ 'data': data })
-  })
+router.use(function(req, res, next){
+  next()
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+router.get('/', function(req, res) {
+	res.json({ message: 'Gramscrap!' })
+})
+
+router.route('/hashtag/:tag')
+  // create a hashtag (accessed at GET http://localhost:8080/api/hashtag/:tag)
+  .get(function(req, res){
+    let hashTag = req.params.tag
+    scragram.getLinksTopPost(hashTag, (err, data) => {
+      err ? res.json({ 'err': err }):res.json({ 'data': data })
+    })
+  })
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router)
+
+// START THE SERVER
+// =============================================================================
+app.listen(port, function () {
+  console.log(`App listening on port ${port}!`)
 })
